f5f6e186aca4bf40ed4102eca7c332061a420078
kirtientomo <Kirti.S@entomo.co>
2025-06-16 13:08:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 DB_CONFIG = {
     'host': 'pocforetlteam.ctanibxecydf.ap-southeast-1.rds.amazonaws.com',
-    'port': 3306,
-    'user': 'swaraj',
-    'password': 'SwarajPassword123!',
-    'database': 'pjli_etl'
+    'port': port,
+    'user': 'user',
+    'password': 'password',
+    'database': 'name'
 }
